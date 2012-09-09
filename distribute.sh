db8bf2e4a57c6c563d9578bb45c5a5fac7dfaef4
gclaybur <>
2012-09-09 01:27:26 +0000

    merged in some more settings from AIX/ADT

--- distribute.sh
+++ distribute.sh
@@ -15,34 +15,63 @@ go(){
 }
 
 doAll(){
-  go cp .bash* .prof* ~
-  go tar -cvf dotfiles2009.tar .prof* .bash* .terminfo
+  go cp .bash* .prof* .dir_colors ~
+  go tar -cvf dotfiles2009.tar .prof* .bash* .terminfo .dir_colors tmp
   go scp -P 21 dotfiles2009.tar ogre:
-  go scp -r -P 21 .bash* .prof* .terminfo ogre:
-  go scp -r .bash* .prof* .terminfo jugglingfarmer:
-  go scp -r .bash* .prof* .terminfo grunt:
-  go scp -r .bash* .prof* .terminfo 192.168.1.120:
-  go scp -r .bash* .prof* .terminfo 192.168.1.116:
-  go scp -r .bash* .prof* .terminfo 192.168.1.117:
+  go scp -r -P 21 .bash* .prof* .terminfo .dir_colors ogre:
+  go scp -r .bash* .prof* .terminfo .dir_colors jugglingfarmer:
+  go scp -r .bash* .prof* .terminfo .dir_colors grunt:
+  go scp -r .bash* .prof* .terminfo .dir_colors packers:
+  go scp -r .bash* .prof* .terminfo .dir_colors vikings:
 }
 
 doOne(){
+  go tar -cvf dotfiles2009.tar .prof* .bash* .terminfo .dir_colors tmp .inputrc
   if [  "$1" == "ogre" ]; then
-    go scp -P 21 -r .bash* .prof* .terminfo "$1":
+    go scp -P 21 -r .bash* .prof* .terminfo .dir_colors tmp .inputrc "$1":
   else
-    go scp -r .bash* .prof* .terminfo "$1":
+    echo "$1" | grep : > /dev/null
+    if [ "$?" == "0" ]; then # assume $1 is of the form: rbeatty@jaxaf2661:gclaybur/
+      USER_DIR="$1"
+      USER_SPEC=$(echo "$1" | cut -d: -f1)
+      USER_HIJACK=$(echo "$1" | cut -d: -f2)
+      go ssh ${USER_SPEC} "mkdir $USER_HIJACK"
+    else  #append : to specify home directory of user on remote host
+      USER_DIR="$1":
+    fi
+    go scp -rp bin .bash* .prof* .terminfo .dir_colors tmp .inputrc "${USER_DIR}"
+#    go ssh "$1" chmod 644 .profile
   fi
+}
+
+installID(){
+  echo "installid: $1"
+  cat ~/.ssh/id_dsa.pub | ssh "$1" -l "$2" "mkdir .ssh; chmod 700 .ssh; cat >> .ssh/authorized_keys"
+  status=$?
+  if [ "$status" != "0" ]; then 
+    exit "$?"
+  fi
+  echo "sshkeys status: $status"
+
 }
 
 usage(){
   echo "$0: must specify an option:"
   echo "  -a  distribute to all known hosts"
   echo " [ hostname ] distribute to hostname"
+  echo " ex 1: copy dotfiles to sy00084 under the gclaybur directory in the rbeatty account" 
+  echo "       $0 rbeatty@sy00084:gclaybur/"    
+  echo " ex 2: copy dotfiles and install ssh public key for gclaybur account on sy02001"
+  echo "       $0 -id sy02001"
+  echo " ex 3: copy dotfiles to gclaybur account on sy02001"
+  echo "       $0 sy02001"
   exit 1
 }
 
 echo "num: $#"
-if [ "$#" -eq "1"  ]; then
+if [ "$1" = "-?" -o "$1" = "-h" ]; then
+  usage
+elif [ "$#" -eq "1"  ]; then
   case "$1" in
     -a)
       doAll
@@ -51,6 +80,18 @@ if [ "$#" -eq "1"  ]; then
       doOne "$1"
       ;;
   esac
+elif [ "$#" -eq "3" ]; then
+  case "$1" in
+    -id)
+      shift
+      installID "$1" "$2"
+      doOne "$1"
+      ./changepass.ex "$1"
+      ;;
+    *)
+      usage
+      ;;
+  esac 
 else
   usage
 fi
