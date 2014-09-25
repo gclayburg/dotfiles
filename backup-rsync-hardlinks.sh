2440919568b071923e798676285e2ab29e27e262
gclaybur <>
2014-09-25 20:46:44 +0000

    script changes to support Solaris date command, now excludes patterns from file

--- backup-rsync-hardlinks.sh
+++ backup-rsync-hardlinks.sh
@@ -2,6 +2,7 @@
 SOURCE_DIR=/export/home/gclaybur/
 BACKUP_HOME=/mnt/backups/ogre-export-home-gclaybur/
 date=`date "+%Y-%m-%d_%H-%M-%S"`
+RUNDIR=$( cd "$( dirname "$0" )" && pwd )
 
 GETOPTS_ARGS="s:d:"
 if ( ! getopts $GETOPTS_ARGS opt ); then
@@ -25,12 +26,12 @@ fi
 do_shell(){
 # Execute command in shell, while logging complete command to stdout
     if [[ ! ${COMMANDS_DISABLED} ]]; then
-      echo "$(date +%F_%T) --> $*"
+      echo "$(date +%Y-%m-%d_%T) --> $*"
       eval "$*"
       STATUS=$?
 #      echo "status= $STATUS"
     else
-      echo "[not executed] $(date +%F_%T) --> $*"
+      echo "[not executed] $(date +%Y-%m-%d_%T) --> $*"
     fi
     return $STATUS
 }
@@ -41,7 +42,7 @@ if [[ -L ${BACKUP_HOME}/current ]]; then
   echo "Performing incremental backup:"
   echo "    source: $SOURCE_DIR"
   echo "    destination: $BACKUP_HOME"
-  do_shell rsync -HaxP --stats --link-dest=${BACKUP_HOME}/current ${SOURCE_DIR} ${BACKUP_HOME}/back-$date
+  do_shell rsync -HaxP --stats --exclude-from=${RUNDIR}/exclude-files.txt --link-dest=${BACKUP_HOME}/current ${SOURCE_DIR} ${BACKUP_HOME}/back-$date
   do_shell rm -f ${BACKUP_HOME}/current
   do_shell ln -s back-$date ${BACKUP_HOME}/current
 else
@@ -50,7 +51,7 @@ else
   echo "    source: $SOURCE_DIR"
   echo "    destination: $BACKUP_HOME"
   if [[ -w ${BACKUP_HOME} ]]; then
-    do_shell rsync -HaxP --stats ${SOURCE_DIR} ${BACKUP_HOME}/back-$date
+    do_shell rsync -HaxP --stats --exclude-from=${RUNDIR}/exclude-files.txt ${SOURCE_DIR} ${BACKUP_HOME}/back-$date
     do_shell ln -s back-$date ${BACKUP_HOME}/current
   else
     echo "Cannot write to: ${BACKUP_HOME}"
