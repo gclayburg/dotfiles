91d51e16da2b6500541d604f7442ea747a5fc147
Gary Clayburg <gclaybur@comcast.net>
2023-11-10 14:06:26 -0700

    gitpod integration changes

--- install.sh
+++ install.sh
@@ -2,29 +2,10 @@
 # this file is only meant for making these dotfiles usable on gitpod.io
 
 current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-echo "I am running from: $current_dir"
-
-dotfiles_source="${current_dir}/home_files"
-
-while read -r file; do
-
-    relative_file_path="${file#"${dotfiles_source}"/}"
-    target_file="${HOME}/${relative_file_path}"
-    target_dir="${target_file%/*}"
-
-    if test ! -d "${target_dir}"; then
-      echo "mkdir ${target_dir} "
-      mkdir -p "${target_dir}"
-    fi
-
-    printf 'Installing dotfiles symlink %s\n' "${file}  ${target_file}"
-    ln -sf "${file}" "${target_file}"
-
-done <   <(echo -e ".bashrc\n.profile")
-#done < <(find "${dotfiles_source}" -type f)
+echo "Installing dotfiles from: $current_dir"
 
 #on gitpod, this repo will be checked out to /home/gitpod/.dotfiles
-# so this script will create symlinks like this:
-# $ ln -sf /home/gitpod/.dotfiles/.bashrc /home/gitpod/.bashrc
-# $ ln -sf /home/gitpod/.dotfiles/.profile /home/gitpod/.profile
+
+ln -sf "${current_dir}/.bashrc" "${HOME}/.bashrc"
+ln -sf "${current_dir}/.profile" "${HOME}/.profile"
 
